f40e9a2466d71a57e7b15f16161e4d6c1cb0ea36
george <george.kvrivi@gmail.com>
2022-08-12 11:26:13 +0400

    update socials

--- src/components/home/socials.jsx
+++ src/components/home/socials.jsx
@@ -1,26 +1,26 @@
 import { SiGithub, SiLinkedin, SiTwitter, SiGmail } from 'react-icons/si';
 import styles from './socials.module.css';
 const Socials = () => {
-  return (
-    <ul className={styles.list}>
-      <a href="https://github.com/drainerr" target="_blank" rel="noreferrer">
-        <SiGithub />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/giorgi-kvrivishvili-5667321b4"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <SiLinkedin />
-      </a>
-      <a href="https://twitter.com/georgeavee" target="_blank" rel="noreferrer">
-        <SiTwitter />
-      </a>
-      <a href="mailto:george.kvrivi@gmail.com">
-        <SiGmail />
-      </a>
-    </ul>
-  );
+	return (
+		<ul className={styles.list}>
+			<a href='https://github.com/drainerr' target='_blank' rel='noreferrer'>
+				<SiGithub />
+			</a>
+			<a
+				href='https://www.linkedin.com/in/giorgi-kvrivishvili-5667321b4'
+				target='_blank'
+				rel='noreferrer'
+			>
+				<SiLinkedin />
+			</a>
+			<a href='https://twitter.com/awemarou' target='_blank' rel='noreferrer'>
+				<SiTwitter />
+			</a>
+			<a href='mailto:george.kvrivi@gmail.com'>
+				<SiGmail />
+			</a>
+		</ul>
+	);
 };
 
 export default Socials;
