fecea32f7a7913035d1b54c296b4434771e78c93
georgekvr <george.kvrivi@gmail.com>
2023-07-22 19:48:57 +0400

    chore: remove unused code

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,25 +1,21 @@
 import { NavLink } from 'react-router-dom';
-// import Resume from '../../Resume.pdf';
 
 const Navbar = (props) => {
-  const { getNavbar, setNavbar } = props;
-  const handleClick = () => setNavbar((prev) => !prev);
-  return (
-    <div className={getNavbar ? 'navigation active' : 'navigation'}>
-      <NavLink to="/" onClick={handleClick}>
-        Home
-      </NavLink>
-      <NavLink to="/Projects" onClick={handleClick}>
-        Projects
-      </NavLink>
-      {/* <a href={Resume} download onClick={handleClick}>
-        Resume
-      </a> */}
-      <NavLink to="/About" onClick={handleClick}>
-        About
-      </NavLink>
-    </div>
-  );
+	const { getNavbar, setNavbar } = props;
+	const handleClick = () => setNavbar((prev) => !prev);
+	return (
+		<div className={getNavbar ? 'navigation active' : 'navigation'}>
+			<NavLink to='/' onClick={handleClick}>
+				Home
+			</NavLink>
+			<NavLink to='/Projects' onClick={handleClick}>
+				Projects
+			</NavLink>
+			<NavLink to='/About' onClick={handleClick}>
+				About
+			</NavLink>
+		</div>
+	);
 };
 
 export default Navbar;
