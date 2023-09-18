64e19714596031b156c6e4b82d63f331cc179035
geeko <george.kvrivi@gmail.com>
2023-09-19 00:21:16 +0400

    change about paragraph style

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,7 +5,7 @@ import { SiLaravel } from 'react-icons/si';
 import { SiVuedotjs } from 'react-icons/si';
 import { SiTailwindcss } from 'react-icons/si';
 import { GrNode } from 'react-icons/gr';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const skillsAsElements = [
 	<SiHtml5 className={styles.html} />,
@@ -23,24 +23,36 @@ const skillsAsElements = [
 ];
 
 const About = () => {
+	const navigate = useNavigate();
+
+	const navigateToProjects = (e) => {
+		if (e.key === 'Enter') {
+			navigate('/projects');
+		}
+	};
+
 	return (
 		<div className={styles.about}>
-			<h1>
-				:<span className={styles.user}>~</span>$ whoam
-				<span className={styles.lastlet}>i</span>
-			</h1>
-			<p>
-				I'm <strong>Giorgi Kvrivishvili</strong>, an Information Technology graduate who decided in 2021 to learn
-				Web Development and dive into that field. So far my knowledge covers the fundamental concepts of Web,{' '}
-				<b>HTML</b>, <b>CSS</b> (SASS/Tailwind), <b>Javascript</b>, <b>Typescript</b>, <b>React</b> and <b>Vue</b>.
-				I'm highly focused on building reactive client-side apps, but besides working on a user interface, I create
-				secure REST APIs using <b>Node/Express</b> and <b>Laravel</b>. I strive to improve and broaden my skill set
-				by working on various kinds of personal projects. You can see some of them{' '}
-				<Link to={'/projects'}>
-					<i>here</i>
-				</Link>
-				.
-			</p>
+			<div className={styles.terminal_wrapper} tabIndex={-1} onKeyDown={navigateToProjects}>
+				<h1>
+					:<span className={styles.user}>~</span>$ cat about
+				</h1>
+				<p>
+					I'm <strong>Giorgi Kvrivishvili</strong>, an Information Technology graduate who decided in 2021 to learn
+					Web Development and dive into that field. So far my knowledge covers the fundamental concepts of Web,{' '}
+					<b>HTML</b>, <b>CSS</b> (SASS/Tailwind), <b>Javascript</b>, <b>Typescript</b>, <b>React</b> and{' '}
+					<b>Vue</b>. I'm highly focused on building reactive client-side apps, but besides working on a user
+					interface, I create secure REST APIs using <b>Node/Express</b> and <b>Laravel</b>. I strive to improve
+					and broaden my skill set by working on various kinds of personal projects. You can see some of them{' '}
+					<Link to={'/projects'}>
+						<i>here</i>
+					</Link>
+				</p>
+				<h1>
+					:<span className={styles.user}>~</span>$ cd projec
+					<span className={styles.lastlet}>ts</span>
+				</h1>
+			</div>
 			<ul className={styles.skills}>
 				{skillsAsElements.map((skill, i) => {
 					return <li key={i}>{skill}</li>;
