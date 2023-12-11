8fc1ac7bba49d384880c12616397816636782a83
geeko <george.kvrivi@gmail.com>
2023-12-11 22:10:57 +0400

    fix: get back the missing schema type

--- src/sanity/schemas/home.ts
+++ src/sanity/schemas/home.ts
@@ -1,22 +1,29 @@
 export default {
-  name: 'home',
-  type: 'document',
-  title: 'Home',
-  fields: [
-    {
-      name: 'greeting',
-      type: 'string',
-      title: 'Greeting',
-    },
-    {
-      name: 'name',
-      type: 'string',
-      title: 'Name',
-    },
-    {
-      name: 'specialty',
-      type: 'string',
-      title: 'Specialty',
-    },
-  ],
-}
+	name: 'home',
+	type: 'document',
+	title: 'Home',
+
+	fields: [
+		{
+			name: 'greeting',
+			type: 'string',
+			title: 'Greeting',
+		},
+		{
+			name: 'name',
+			type: 'string',
+			title: 'Name',
+		},
+		{
+			name: 'specialty',
+			type: 'string',
+			title: 'Specialty',
+		},
+		{
+			name: 'socialLinks',
+			title: 'Social Links',
+			type: 'array',
+			of: [{ type: 'string' }],
+		},
+	],
+};
